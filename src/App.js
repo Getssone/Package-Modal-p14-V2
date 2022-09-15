b232cad448b093cdf0b1c0b6ec02288c951eb465
Solis <gaetan.solis@gmail.com>
2022-09-15 21:39:34 +0200

    Creation Modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Modal from './lib/components/Modal';
+
+
+const modal = "Employee Created! It's Me Luigi 😉✅"
+const close = "Here We Go ! 🚩"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Modal texte={modal} close={close} />/
+        </div>
+    );
 }
 
 export default App;
---
+++ src/lib/components/Modal.jsx
@@ -0,0 +1,37 @@
+import "./Modalcss.css";
+
+export default function Modal({ texte, close }) {
+  document.addEventListener(
+    "click",
+
+    function (event) {
+      // If user either clicks on the button OR clicks outside the modal window, then close modal by calling closeModal()
+      if (
+        event.target.matches(".button-close-modal") ||
+        !event.target.closest(".ModalContainer")
+      ) {
+        closeModal();
+      }
+    },
+    false
+  );
+
+  function closeModal() {
+    document.querySelector(".ModalContainer").style.display = "none";
+  }
+
+  return (
+    <>
+      <div className="ModalContainer">
+        <div className="ModalMessage">
+          <p> {texte} </p>
+          <div className="ModalButton">
+            <button className="button-close-modal" onClick={closeModal}>
+              {close}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
